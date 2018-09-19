6322ee8fe62bbf87bb6563f4d8f4821d7cf9eab8
汪春婷 <wangchunting@51zan.com>
2018-09-19 15:39:41 +0800

    fix1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
-                    aaa <code>初始化</code>cccc
+                    aaa <code>初始化</code>fix1
                </p>
             </div>
         );
