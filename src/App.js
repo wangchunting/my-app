521ca2e29d93aa0216c64b9af78efad22d982e8d
汪春婷 <wangchunting@51zan.com>
2018-09-19 15:28:28 +0800

    修改文案

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
-                    aaa <code>bbb</code>cccc
+                    aaa <code>初始化</code>cccc
                </p>
             </div>
         );
